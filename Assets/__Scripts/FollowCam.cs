aedab2eba4f8a16a5142e3ffde6294dca7ee1287
Joe Sang Nguyen <sang.nguyen@rutgers.edu>
2015-06-23 14:55:16 -0400

    First

---
+++ Assets/__Scripts/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	static public FollowCam S;
+
+	public bool ____________________________;
+	public GameObject poi; 
+	public float camZ;
+
+	void Awake() {
+				S = this;
+				camZ = this.transform.position.z;
+		}
+
+	void update () {
+				if (poi == null)
+						return;
+				Vector3 destination = poi.transform.position;
+				destination.z = camZ;
+				transform.position = destination;
+		}
+	
+}
